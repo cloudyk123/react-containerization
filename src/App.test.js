a8d8670d83ed03d78320d64cdf790e28db04f0f0
cloudyk123 <yusungkung35@gmail.com>
2023-07-23 22:12:50 +0800

    add back test to demo the devops life cycle

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-//
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import {render, screen} from '@testing-library/react';
+import App from './App';
+import Time from './views/Timeline';
+
+//write test about the timeline
+test('renders timeline', () => {
+    render(<Time/>);
+    const linkElement = screen.getByText(/The event of 1 Jan 2021 : Happy New Year/i);
+    expect(linkElement).toBeInTheDocument();
+});
--- src/views/Timeline.js
+++ src/views/Timeline.js
@@ -1,27 +1,28 @@
 import React, {useState} from 'react';
 import HorizontalTimeline from 'react-horizontal-timeline';
 import "./timeline.css";
-import image1 from "../images/image1.png";
-import image2 from "../images/image1.png";
+import image1 from "../images/1-save-on-cost-overruns-using-finops-powered-advanced-data-integration-services.png";
+import image2 from "../images/devsecops.jpg";
 import image3 from "../images/image1.png";
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 
-const VALUES = ["2021-01-01", "2021-01-15", "2021-03-22", "2021-03-23", "2021-03-24", "2021-03-25", "2021-03-26", "2021-03-27"];
+const VALUES = ["2021-03-23", "2021-03-24", "2021-03-25"];
 
 // Description array corresponding to values
 const description = [
     "The event of 1 Jan 2021 : Happy New Year",
     "The event of 15 Jan 2021 : Festival",
-    "The event of 22 March 2021 : Board Exam 中文可以嗎",
+    "The event of 22 March 2021 : Board Exam",
 ];
 
 const images = [image1, image2, image3];
+const content = ["Hello", "This", "is", "a", "basic", "test"];
 
 function Time() {
 
     const [state, setState] = useState({value: 0, previous: 0});
     const fillingMotion = {stiffness: 150, damping: 50};
-    const styles = { background: '#f8f8f8', foreground: '#7b9d6f', outline: '#dfdfdf'};
+    const styles = {background: '#f8f8f8', foreground: '#7b9d6f', outline: '#dfdfdf'};
     return (
 
         <div>
@@ -30,23 +31,27 @@ function Time() {
                 <HorizontalTimeline
                     index={state.value}
                     indexClick={(index) => {
-                        setState({ value: index, previous: state.value });
+                        setState({value: index, previous: state.value});
                     }}
-                    values={ VALUES }
-                    fillingMotion = {fillingMotion}
-                    styles = {styles}
-                    />
+                    values={VALUES}
+                    fillingMotion={fillingMotion}
+                    styles={styles}
+                />
 
             </div>
-            <div className='text-center'>
-                {/* any arbitrary component can go here */}
-                {description[state.value]}
-            </div>
-            <div>
-                <img src={images[state.value]} className="image" alt="pic" />
+            <div className='container'>
+                <div className='text-center'>
+                    {/* any arbitrary component can go here */}
+                    <div className="title">{description[state.value]}</div>
+                    <div className="content">{content[state.value]}</div>
+                </div>
+                <div>
+                    <img src={images[state.value]} className="image" alt="pic"/>
+                </div>
             </div>
         </div>
     );
 }
+
 // ReactDOM.render(<Timeline events={events} />, document.getElementById('root'));
 export default Time;
